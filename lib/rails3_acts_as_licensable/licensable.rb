0c355768161ecc94b95d21756489232fe4317ca8
drn <drn@ecs.soton.ac.uk>
2012-02-07 20:02:46 +0000

    Updated README. Made acts_as_licensable rather than is_licensable

--- lib/rails3_acts_as_licensable/licensable.rb
+++ lib/rails3_acts_as_licensable/licensable.rb
@@ -1,6 +1,6 @@
 module Rails3ActsAsLicensable
   module Licensable
-    def is_licensable
+    def acts_as_licensable
         has_one :licensing, :class_name => "Rails3ActsAsLicensable::Licensing", :as => :licensable, :dependent => :destroy, :include => :license
         has_one :license, :class_name => "Rails3ActsAsLicensable::License", :through => :licensing, :source => :license
         include InstanceMethods
